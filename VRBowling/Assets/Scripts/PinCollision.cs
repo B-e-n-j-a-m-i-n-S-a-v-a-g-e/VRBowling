ce8c20f1d365a39b6baf97a4128b28a7971e2886
Ben Savage <Finnegan2100@protonmail.com>
2018-07-16 21:31:41 -0700

    Created basic "alley". Organized assets folder a bit.

---
+++ VRBowling/Assets/Scripts/PinCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinCollision : MonoBehaviour {
+
+	public Pin pin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "floor") {
+			pin.OnTouchFloor ();
+		}
+	}
+}
---
+++ VRBowling/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public GameObject BowlingBall;
+	public GameObject Arrow;
+	public float ballDistance = 2.5f;
+	public float arrowDistance = 2.5f;
+
+	public bool holding = true;
+	public float ballThrowingForce = 200.0f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		if (holding) {
+			BowlingBall.transform.position = transform.position + Camera.main.transform.forward * ballDistance;
+			Arrow.transform.position = transform.position + 
+				new Vector3(0.0f, BowlingBall.transform.position.y - 4.3f, BowlingBall.transform.position.z);
+
+			Arrow.transform.eulerAngles = new Vector3 (0.0f, transform.eulerAngles.y, 0.0f);
+
+			if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began || Input.GetKeyDown ("space")) {
+				holding = false;
+				BowlingBall.GetComponent<Rigidbody> ().useGravity = true;
+				BowlingBall.GetComponent<Rigidbody> ().AddForce (transform.forward * ballThrowingForce);
+			}
+		}
+
+		if (BowlingBall.transform.position.y < -10) {
+			BowlingBall.transform.position = new Vector3 (0, 2, 5);
+			holding = true;
+		}
+
+	}
+}
