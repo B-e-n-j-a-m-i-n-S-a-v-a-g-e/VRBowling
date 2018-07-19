fded2aa2886c988ac37bab3e7269a43260c7a260
Ben Savage <Finnegan2100@protonmail.com>
2018-07-18 22:46:37 -0700

    Created Basic Scene Controller in Ball Selection Scene & basic ability to switch to Main Bowling Scene.

---
+++ VRBowling/Assets/Scripts/BallSelectionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallSelectionController : MonoBehaviour {
+
+	private string currentBall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.transform.name == "FieriBall") {
+					Debug.Log ("ok.");
+					currentBall = "FieriBall";
+					SceneManager.LoadScene ("HelloVR");
+				}
+			}
+		}
+	}
+}
