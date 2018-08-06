a836355ef9fb552a23c07caf79dbd823aaf76db2
Ben Savage <Finnegan2100@protonmail.com>
2018-08-05 22:06:13 -0700

    Added camera rotation effect in title screen.

--- VRBowling/Assets/Scripts/BallSelectionController.cs
+++ VRBowling/Assets/Scripts/BallSelectionController.cs
@@ -6,25 +6,48 @@ using UnityEngine.SceneManagement;
 public class BallSelectionController : MonoBehaviour {
 
 	private string currentBall;
+	private bool isShowingTitle = false;
 
-	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.transform.name == "FieriBall") {
-					Debug.Log ("ok.");
-					currentBall = "FieriBall";
-					SceneManager.LoadScene ("Gameplay");
+		StartCoroutine ("MoveCamera");
+	}
+
+	void Update () {
+
+		if (isShowingTitle) {
+
+			if (Camera.main.gameObject.transform.rotation.y <= 0.0f) {
+				
+				Debug.Log (Camera.main.gameObject.transform.rotation.y);
+				Camera.main.gameObject.transform.Rotate (0, 0.2f, 0);
+			} else if (Camera.main.gameObject.transform.rotation.y <= 0.0f) {  
+				
+				GameObject.Find ("IntroScreenText").SetActive (true);
+				GameObject.Find ("TitleText").SetActive (false);
+			}
+				
+			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+				if (Physics.Raycast (ray, out hit)) {
+					if (hit.transform.name == "FieriBall") {
+						Debug.Log ("ok.");
+						currentBall = "FieriBall";
+						SceneManager.LoadScene ("Gameplay");
+					}
 				}
 			}
+		} else {
+			GameObject.Find ("IntroScreenText").SetActive (false);
+			GameObject.Find ("TitleText").SetActive (true);
 		}
+	}
+
+	IEnumerator MoveCamera() {
+
+		yield return new WaitForSeconds (3.0f);
+		isShowingTitle = true;
 	}
 }
