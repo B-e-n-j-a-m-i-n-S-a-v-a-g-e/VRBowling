96ed4586cc9857cf107f9c570e52c80208519ad7
Ben Savage <Finnegan2100@protonmail.com>
2018-08-19 22:13:08 -0700

    Tweaking smoke effect. Reduced num particles to 1, enlarged and placed center on top of camera.

---
+++ VRBowling/Assets/Scripts/SmokeMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3 (0.03f, 0, 0) * Time.deltaTime;
+
+	}
+}
