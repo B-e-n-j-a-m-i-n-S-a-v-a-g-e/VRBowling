313d1a6d1035ce3cbb0977bc8842c89a93c312f4
Ben Savage <Finnegan2100@protonmail.com>
2018-08-21 22:16:03 -0700

    Added silly font with "strike" text, created animation for it and placed it above the audience's heads.

--- VRBowling/Assets/Scripts/TriggerCameraChange.cs
+++ VRBowling/Assets/Scripts/TriggerCameraChange.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class TriggerCameraChange : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject whiteImage;
 
 	void OnTriggerEnter(Collider collider) {
-		whiteImage.SetActive (false);
 
 		if (collider.tag == "Ball") {
 			player.transform.position = new Vector3 (-0.1f, -0.26f, 22.39f);
