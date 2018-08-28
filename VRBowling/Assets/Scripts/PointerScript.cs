01be3c08229b10d1597c9118fbdefab510cebd06
Ben Savage <Finnegan2100@protonmail.com>
2018-08-27 22:24:25 -0700

    Added segue into gameplay scene from ball selection menu.

---
+++ VRBowling/Assets/Scripts/PointerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision col) {
+
+
+		Debug.Log (col.gameObject.name);
+
+		switch(col.gameObject.name)
+		{
+		case "FieriBall":
+			Debug.Log ("Fieri collision");
+			break;
+		}
+	}
+}
